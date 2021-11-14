d641a988f181a4527a67569adaf54b9c1dbb6083
SikoraKam <sikorakam3@gmail.com>
2021-11-14 14:17:08 +0100

    added filter for providers

--- src/hooks/user/useProvidersByStartPlaceId.ts
+++ src/hooks/user/useProvidersByStartPlaceId.ts
@@ -3,6 +3,7 @@ import { UserObject } from "../../types/user/UserObject";
 import { QUERY_PROVIDERS } from "../../constants/queryConstants";
 import { fetcher } from "../../utils/fetcher";
 
+//currently not used
 export const useProvidersByStartPlaceId = (startPlaceId: string | null) => {
   const { data, error, mutate } = useSWR<UserObject[]>(
     startPlaceId ? `${QUERY_PROVIDERS}/${startPlaceId}` : null,
